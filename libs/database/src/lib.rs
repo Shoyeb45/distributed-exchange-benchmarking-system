0763b1737e580725f0a8eb951f8ef34d471ae261
shoyeb45 <shoyebff45@gmail.com>
2026-06-09 18:22:30 +0530

    added db support using cornucpia

--- libs/database/src/lib.rs
+++ libs/database/src/lib.rs
@@ -1,14 +1,14 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use crate::generated::queries::query::{get_submission_by_id, Submission};
+use deadpool_postgres::Pool;
+use uuid::Uuid;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub async fn fetch_submission(pool: &Pool, id: Uuid) -> Result<Option<Submission>, Error> {
+    let client = pool.get().await?;
+    
+    let submission = get_submission_by_id()
+        .bind(&client, &id)
+        .opt()  // use .one() if always exists, .all() for Vec
+        .await?;
+    
+    Ok(submission)
 }
